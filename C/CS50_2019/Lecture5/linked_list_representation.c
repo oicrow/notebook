e7f8a8b69db2c9b05d67e8c4f2dfbc47a9208ea4
croreo <77658038+croreo@users.noreply.github.com>
2022-01-27 21:51:25 +0900

    Add files via upload

--- C/CS50_2019/Lecture5/linked_list_representation.c
+++ C/CS50_2019/Lecture5/linked_list_representation.c
@@ -2,22 +2,76 @@
 //           : NUL is character '\0', represented with binary 0
 
 // + linked list can resize list so easily
-//     -> insert: O(1)
-// - but since you alwaysneed to access from the root node along the list, only linear search is available
+//     -> insert: O(n)
+// - but since you always need to access from the root node along the list, only linear search is available
 //     -> search: O(n)
 // - also, you need to sacrifice more memory to store the pointer to the next node
 
 
 #include <stdio.h>
+#include <stdlib.h>     // malloc, free
 
-typedef struct node
-{
-    int number;
-    struct node *next;
+typedef struct node     // <--
+{                       //   | two 'node's must be
+    int number;         //   | synchronized
+    struct node *next;  // <--
 }
-node;
+node;                   // <-- 'node' here doens't have to be same, but conventionally it is
 
 int main(void)
 {
+    // Create empty list
+    node *list = NULL;
 
+    // Make 1st node and store value
+    node *n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        return 1;
+    }
+    (*n).number = 2;    // same as: n->number = 2;
+    n->next = NULL;
+    // Link it to the list
+    list = n;
+
+    // Make 2nd node and store value
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list);
+        return 1;
+    }
+    n->number = 4;
+    n->next = NULL;
+    // Link it to the list
+    list->next = n;
+
+    // Make 3rd node and store value
+    n = malloc(sizeof(node));
+    if (n == NULL)
+    {
+        free(list->next);
+        free(list);
+        return 1;
+    }
+    n->number = 4;
+    n->next = NULL;
+    // Link it to the list
+    list->next->next = n;
+
+
+    // print list
+    for (node *tmp = list; tmp != NULL; tmp = tmp->next)
+    {
+        printf("%i\n", tmp->number);
+    }
+
+
+    // free list
+    while (list != NULL)
+    {
+        node *tmp = list->next;
+        free(list);
+        list = tmp;
+    }
 }
