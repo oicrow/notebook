d3e20a977d692ddd64ca9bd5b07a38cafad89d04
croreo <77658038+croreo@users.noreply.github.com>
2022-02-26 18:10:21 +0900

    Add files via upload

--- C/CS50_2019/Lecture5/dictionary.c
+++ C/CS50_2019/Lecture5/dictionary.c
@@ -2,3 +2,22 @@
 //  : data structure that has keys and values (anything that maps one to another)
 //  : using hash table, hash functions
 //  : also available in python
+
+#include <stdio.h>
+#include <stdlib.h> // malloc, free???
+
+typedef struct Entry
+{
+     *key;
+    char *value;
+} Entry;
+
+typedef struct Dictionary
+{
+    
+} Dictionary;
+
+int main(void)
+{
+    
+}
