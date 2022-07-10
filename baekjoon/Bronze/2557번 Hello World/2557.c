f922b012fdac12b496b5bc290357a8441ea6f660
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-11 02:43:58 +0900

    Create 2557.c

---
+++ baekjoon/Bronze/2557번 Hello World/2557.c
@@ -0,0 +1,12 @@
+// Date         : 2022-07-11
+// Problem      : No. 2557 Hello World (https://www.acmicpc.net/problem/2557)
+// Step         : 1
+// Language     : C99
+// Source Link  : https://www.acmicpc.net/source/45847893
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!");
+}
