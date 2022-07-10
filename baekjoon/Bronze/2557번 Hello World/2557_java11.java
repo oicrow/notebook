b5dc12131a3d4adc5a49c8e4a08067f363b6beed
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-11 02:48:47 +0900

    Create 2557_java11.java

---
+++ baekjoon/Bronze/2557번 Hello World/2557_java11.java
@@ -0,0 +1,11 @@
+// Date         : 2022-07-11
+// Problem      : No. 2557 Hello World (https://www.acmicpc.net/problem/2557)
+// Step         : 1
+// Language     : java11
+// Source Link  : https://www.acmicpc.net/source/45847977
+
+public class Main{
+    public static void main(String[] agrs){
+        System.out.println("Hello World!");
+    }
+}
