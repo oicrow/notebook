dff2ced538b259b329f37e782701cbbf26d4d5ff
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-11 03:13:14 +0900

    Create HelloWorldApp.java

---
+++ boostcourse_java1/section 1 자바 프로그래밍 소개/Ch4/HelloWorldApp.java
@@ -0,0 +1,9 @@
+// Date Created: 2022-07-11
+// Date Edited : 2022-07-11
+// Chapter     : Ch4-1. 실행_HelloWorld
+
+public class HelloWorldApp {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+}
