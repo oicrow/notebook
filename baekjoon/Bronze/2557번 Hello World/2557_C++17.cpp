83bf1d1c55d8bd54922ce2f4380e62ee3585cee5
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-11 02:47:32 +0900

    Create 2557_C++17.cpp

---
+++ baekjoon/Bronze/2557번 Hello World/2557_C++17.cpp
@@ -0,0 +1,12 @@
+// Date         : 2022-07-11
+// Problem      : No. 2557 Hello World (https://www.acmicpc.net/problem/2557)
+// Step         : 1
+// Language     : C++17
+// Source Link  : https://www.acmicpc.net/source/45847918
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello World!";
+}
