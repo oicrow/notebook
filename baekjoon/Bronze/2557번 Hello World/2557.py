d4e6be129c193f0eb3ee0b05731ed8faebcb997f
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-11 02:45:21 +0900

    Create 2557.py

---
+++ baekjoon/Bronze/2557번 Hello World/2557.py
@@ -0,0 +1,7 @@
+# Date         : 2022-07-11
+# Problem      : No. 2557 Hello World (https://www.acmicpc.net/problem/2557)
+# Step         : 1
+# Language     : Python 3
+# Source Link  : https://www.acmicpc.net/source/45847901
+
+print("Hello World!")
