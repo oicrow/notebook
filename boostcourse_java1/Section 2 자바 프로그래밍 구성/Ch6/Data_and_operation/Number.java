cdcc462ed1fb6bbdeecc6d59a6248f2474f6312c
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 23:25:56 +0900

    Create Number.java

---
+++ boostcourse_java1/Section 2 자바 프로그래밍 구성/Ch6/Data_and_operation/Number.java
@@ -0,0 +1,22 @@
+// Date Created: 2022-07-14
+// Date Edited : 2022-07-14
+// Chapter     : Ch6-3. 숫자와 연산
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(6+2); // 8
+		System.out.println(6-2); // 4
+		System.out.println(6*2); // 12
+		System.out.println(6/2); // 3
+		
+		System.out.println(Math.PI); // pi = 3.141592...
+		System.out.println(Math.floor(Math.PI)); // floor operation
+		System.out.println(Math.ceil(Math.PI)); // ceiling operation
+		
+		// Math: a class containing operators, constants, etc
+		// document: https://docs.oracle.com/javase/8/docs/api/java/lang/Math.html
+
+	}
+
+}
