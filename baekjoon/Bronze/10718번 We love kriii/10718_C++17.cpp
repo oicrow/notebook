46d312a6ebb4962fd2c0929db8936df542406be7
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 19:09:32 +0900

    Create 10718_C++17.cpp

---
+++ baekjoon/Bronze/10718번 We love kriii/10718_C++17.cpp
@@ -0,0 +1,13 @@
+// Date         : 2022-07-14
+// Problem      : No. 10718 We love kriir (https://www.acmicpc.net/problem/10718)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C++17
+// Source Link  : https://www.acmicpc.net/source/46060051
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "강한친구 대한육군\n강한친구 대한육군";
+}
