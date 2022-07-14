a688ae7324c76af477367f5f9731c1ef6667bdca
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 19:11:06 +0900

    Create 10718_Python3.py

---
+++ baekjoon/Bronze/10718번 We love kriii/10718_Python3.py
@@ -0,0 +1,9 @@
+# Date         : 2022-07-14
+# Problem      : No. 10718 We love kriir (https://www.acmicpc.net/problem/10718)
+# Difficulty   : Bronze 5
+# Step         : 1
+# Language     : Python3
+# Source Link  : https://www.acmicpc.net/source/46060155
+
+print("강한친구 대한육군")
+print("강한친구 대한육군")
