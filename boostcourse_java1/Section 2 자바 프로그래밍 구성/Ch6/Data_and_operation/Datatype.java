539d552f3c0ca3c6e8a8cab4e6f2a0b54cef3ee6
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 23:03:48 +0900

    Create Datatype.java

---
+++ boostcourse_java1/Section 2 자바 프로그래밍 구성/Ch6/Data_and_operation/Datatype.java
@@ -0,0 +1,22 @@
+// Date Created: 2022-07-14
+// Date Edited : 2022-07-14
+// Chapter     : Ch6-2. 데이터 타입
+
+public class Datatype{
+	public static void main(String[] agrs) {
+		System.out.println(6);      // 6
+		System.out.println("six");  // six
+		
+		System.out.println("6");    // 6
+		
+    System.out.println(6+6);    // 12
+    System.out.println("6"+"6");// 66
+    
+    System.out.println(6*6);    // 36
+//  System.out.println("6"*"6");
+//  System.out.println("6"*6);
+    
+    System.out.println("1111".length()); // 4
+//  System.out.println(1111.length());
+	}
+}
