f75cd7a13b0af470bf9c9a3ae77b9a5f87e0cb32
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 19:15:15 +0900

    Create 10718_java11.java

---
+++ baekjoon/Bronze/10718번 We love kriii/10718_java11.java
@@ -0,0 +1,13 @@
+// Date         : 2022-07-14
+// Problem      : No. 10718 We love kriir (https://www.acmicpc.net/problem/10718)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : java 11
+// Source Link  : https://www.acmicpc.net/source/46060350
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
