03b1d6afc63e3be6de1a1bc44d4d8502030cd54b
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-14 18:55:38 +0900

    Create 10718_C99.c

---
+++ baekjoon/Bronze/10718번 We love kriii/10718_C99.c
@@ -0,0 +1,13 @@
+// Date         : 2022-07-14
+// Problem      : No. 10718 We love kriii (https://www.acmicpc.net/problem/10718)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C99
+// Source Link  : https://www.acmicpc.net/source/46059418
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("강한친구 대한육군\n강한친구 대한육군");
+}
