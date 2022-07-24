98f6e0841ac5aea131a31ce1d152fb0698bada91
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:09:52 +0900

    Create 10172_Python3.py

---
+++ baekjoon/Bronze/10172번 Dogs/10172_Python3.py
@@ -0,0 +1,8 @@
+# Date         : 2022-07-24
+# Problem      : No. 10172 Dogs (https://www.acmicpc.net/problem/10172)
+# Difficulty   : Bronze 5
+# Step         : 1
+# Language     : Python3
+# Source Link  : https://www.acmicpc.net/source/46584824
+
+print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n")
