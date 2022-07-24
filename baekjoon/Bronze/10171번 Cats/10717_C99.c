a834ea505537902670009c0087e4da9e77d9906c
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 21:44:11 +0900

    Create 10717_C99.c

---
+++ baekjoon/Bronze/10171번 Cats/10717_C99.c
@@ -0,0 +1,16 @@
+// Date         : 2022-07-24
+// Problem      : No. 10171 Cats (https://www.acmicpc.net/problem/10171)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C99
+// Source Link  : https://www.acmicpc.net/source/46583275
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");	
+}
