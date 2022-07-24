fae2d4f56aedf7855e00a76ea6a02c8a8b539672
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:07:32 +0900

    Create 10172_C99.c

---
+++ baekjoon/Bronze/10172번 Dogs/10172_C99.c
@@ -0,0 +1,13 @@
+// Date         : 2022-07-24
+// Problem      : No. 10172 Dogs (https://www.acmicpc.net/problem/10172)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C99
+// Source Link  : https://www.acmicpc.net/source/46584678
+
+#include <stdio.h>
+
+int main(void)
+{
+  printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+}
