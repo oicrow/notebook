ce09b092520b52b3dacb1a357e2f763216ff0250
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:21:17 +0900

    Create 01000_C++17.cpp

---
+++ baekjoon/Bronze/01000번 A+B/01000_C++17.cpp
@@ -0,0 +1,16 @@
+// Date         : 2022-07-24
+// Problem      : No. 01000 A+B (https://www.acmicpc.net/problem/01000)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C++17
+// Source Link  : https://www.acmicpc.net/source/46585494
+
+// #define _CRT_SECURE_NO_WARNINGS // for VS Studio (not required in BOJ)
+#include <iostream>
+
+int main(void)
+{
+	int A, B;
+	scanf("%d %d", &A, &B);
+	std::cout << A + B;
+}
