4fd8faf39039abd0f0ded083a06089a7eed9a5d3
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 21:55:35 +0900

    Create 10171_java11.java

---
+++ baekjoon/Bronze/10171번 Cats/10171_java11.java
@@ -0,0 +1,15 @@
+// Date         : 2022-07-24
+// Problem      : No. 10171 Cats (https://www.acmicpc.net/problem/10171)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : java 11
+// Source Link  : https://www.acmicpc.net/source/46583918
+
+public class Main{
+  public static void main(String[] args){
+    System.out.println("\\    /\\");
+    System.out.println(" )  ( ')");
+    System.out.println("(  /  )");
+    System.out.println(" \\(__)|");
+  }
+}
