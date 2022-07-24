04dc6617c8129f3167da70e8f6e441acf5f3c3dc
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:17:52 +0900

    Create 01000_C99.c

---
+++ baekjoon/Bronze/01000번 A+B/01000_C99.c
@@ -0,0 +1,16 @@
+// Date         : 2022-07-24
+// Problem      : No. 01000 A+B (https://www.acmicpc.net/problem/01000)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C99
+// Source Link  : https://www.acmicpc.net/source/
+
+// #define _CRT_SECURE_NO_WARNINGS // for VS Studio (not required in BOJ)
+#include <stdio.h>
+
+int main(void)
+{
+  int A, B;
+  scanf("%d %d", &A, &B);
+  printf("%d\n", A+B);
+}
