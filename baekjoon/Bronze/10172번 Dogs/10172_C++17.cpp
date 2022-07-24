4940f0744f0dc3410d094659e188b35d0f612c46
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:08:42 +0900

    Create 10172_C++17.cpp

---
+++ baekjoon/Bronze/10172번 Dogs/10172_C++17.cpp
@@ -0,0 +1,13 @@
+// Date         : 2022-07-24
+// Problem      : No. 10172 Dogs (https://www.acmicpc.net/problem/10172)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C++17
+// Source Link  : https://www.acmicpc.net/source/46584745
+
+#include <iostream>
+
+int main(void)
+{
+  std::cout << "|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n";
+}
