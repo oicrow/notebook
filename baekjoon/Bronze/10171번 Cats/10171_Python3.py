804f7f81819571a63812ba97d4c8c8515c84a48b
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 21:50:23 +0900

    Create 10171_Python3.py

---
+++ baekjoon/Bronze/10171번 Cats/10171_Python3.py
@@ -0,0 +1,11 @@
+# Date         : 2022-07-24
+# Problem      : No. 10171 Cats (https://www.acmicpc.net/problem/10171)
+# Difficulty   : Bronze 5
+# Step         : 1
+# Language     : Python 3
+# Source Link  : https://www.acmicpc.net/source/46583681
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")	
