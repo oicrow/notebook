1ed5c42b72e21d05bd896f4b384933cc00268219
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 21:47:14 +0900

    Create 10171_C++17.cpp

---
+++ baekjoon/Bronze/10171번 Cats/10171_C++17.cpp
@@ -0,0 +1,16 @@
+// Date         : 2022-07-24
+// Problem      : No. 10171 Cats (https://www.acmicpc.net/problem/10171)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : C++17
+// Source Link  : https://www.acmicpc.net/source/46583461
+
+#include <iostream>
+
+int main(void)
+{
+	std::cout << "\\    /\\\n";
+	std::cout << " )  ( ')\n";
+	std::cout << "(  /  )\n";
+	std::cout << " \\(__)|\n";	
+}
