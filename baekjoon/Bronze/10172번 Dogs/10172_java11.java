1ea078a256ad519b53a4df2c0b494fb15ef3f322
oicrow <77658038+oicrow@users.noreply.github.com>
2022-07-24 22:11:57 +0900

    Create 10172_java11.java

---
+++ baekjoon/Bronze/10172번 Dogs/10172_java11.java
@@ -0,0 +1,12 @@
+// Date         : 2022-07-24
+// Problem      : No. 10172 Dogs (https://www.acmicpc.net/problem/10172)
+// Difficulty   : Bronze 5
+// Step         : 1
+// Language     : java11
+// Source Link  : https://www.acmicpc.net/source/46584944
+
+public class Main{
+  public static void main(String[] args){
+    System.out.print("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+  }
+}
